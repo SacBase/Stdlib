c7e710d1ebaae23148a0e5c647ff7e3659b271de
Nico Marcussen-Wulff <info@sac-home.org>
2000-08-03 14:18:34 +0000

    function renamed from _drop to drop

--- src/structures/src/List/drop.c
+++ src/structures/src/List/drop.c
@@ -6,7 +6,7 @@
 #include "List.h"
 
 
-void _drop( SAC_ND_KS_DEC_OUT_RC(list *, res), int n, SAC_ND_KS_DEC_IN_RC(list *, elems))
+void drop( SAC_ND_KS_DEC_OUT_RC(list *, res), int n, SAC_ND_KS_DEC_IN_RC(list *, elems))
 {
 /*
  * we do have now:
