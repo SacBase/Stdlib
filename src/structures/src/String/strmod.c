4d557e7a202fcc8b8ba6a6eb10bf2aa572429467
Dietmar Kreye <info@sac-home.org>
2002-03-07 21:21:49 +0000

    ssome ICMs renamed

--- src/structures/src/String/strmod.c
+++ src/structures/src/String/strmod.c
@@ -6,41 +6,35 @@
 #include "StringC.h"
 
 
-
 /*****************************************************************/
 
-void strmod(SAC_ND_KS_DEC_OUT_RC(string, new), 
-				SAC_ND_KS_DEC_IN_RC(string, old),
-				int pos, char c)
+
+void strmod( SAC_ND_PARAM_out_rc( string, new),
+             SAC_ND_PARAM_in_rc( string, old),
+             int pos, char c)
 {
   SAC_ND_DECL_RC(string, new);
 
   RANGECHECK(pos, 0, strlen(old)-1, old);
   
-  if (SAC_ND_A_RC(old)==1)
-  {
-	 new=old;
-	 new[pos]=c;
+  if (SAC_ND_A_RC(old)==1) {
+    new=old;
+    new[pos]=c;
 
-	 SAC_ND_A_RCP(new)=SAC_ND_A_RCP(old);
+    SAC_ND_A_RCP(new)=SAC_ND_A_RCP(old);
   }
-  else
-  {
-	 STRDUP(new, old);
-	 new[pos]=c;
+  else {
+    STRDUP(new, old);
+    new[pos]=c;
 	 
-	 SAC_ND_ALLOC_RC(new);
-	 SAC_ND_SET_RC(new, 1);
+    SAC_ND_ALLOC_RC(new);
+    SAC_ND_SET_RC(new, 1);
 
-	 SAC_ND_DEC_RC(old, 1);
+    SAC_ND_DEC_RC(old, 1);
   }
   
-  SAC_ND_KS_RET_OUT_RC( new, new);
+  SAC_ND_RET_out_rc( new, new);
 }
-  
-	 
+
 
 /*****************************************************************/
-
-
-
