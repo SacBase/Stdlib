c061f54f4702c32c0a939e2518346eee905bd929
Dietmar Kreye <info@sac-home.org>
2002-08-03 01:34:22 +0000

    type of array descriptor modified

--- src/structures/src/List/List.h
+++ src/structures/src/List/List.h
@@ -15,9 +15,9 @@
 #ifdef TAGGED_ARRAYS
      
 typedef struct LIST {
-  int                  elem;
-  struct LIST          *rest;
-  SAC_array_descriptor *desc;
+  int                    elem;
+  struct LIST            *rest;
+  SAC_array_descriptor_t desc;
 } list;
 
 #else
