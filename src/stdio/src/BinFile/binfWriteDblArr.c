b713ade2511b80a76b6f65712b3ab305819d07db
Markus Weigel <mwg@sac-home.org>
2006-11-22 14:33:24 +0000

    - Added functions to read and write double arrays from/to binary files. - Added some common C flags used when opening files

---
+++ src/stdio/src/BinFile/binfWriteDblArr.c
@@ -0,0 +1,23 @@
+/*
+ *  Implementation of module BinFile
+ */
+
+
+#include "BinFile.h"
+
+#define array_nt (array, (AUD, (NHD, (NUQ, ))))
+#define ret_nt   (ret,   (AUD, (NHD, (NUQ, ))))
+
+void SACbinfWriteDoubleArray(int fd, int dim, int *shp, double* array)
+{
+  int bytesWritten;
+  int i,size;
+  for( i = 0, size = 1; i < dim; i++) {
+    size *= shp[i];
+  }
+
+  bytesWritten = write(fd,array,size*sizeof(double));
+}
+
+#undef array_nt
+#undef ret_nt
--- src/stdio/src/BinFile/binfflags.c
+++ src/stdio/src/BinFile/binfflags.c
@@ -15,6 +15,21 @@ int SACbinf_O_RDONLY()
   return(O_RDONLY);
 }
 
+int SACbinf_O_WRONLY()
+{
+  return(O_WRONLY);
+}
+
+int SACbinf_O_RDWR()
+{
+  return(O_RDWR);
+}
+
+int SACbinf_O_CREAT()
+{
+  return(O_CREAT);
+}
+
 
 /*****************************************************************/
 
