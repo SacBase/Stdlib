0dd4b0004a19743516ef7560d08c224966970d08
Stephan Herhut <sah@sac-home.org>
2007-06-20 10:10:17 +0000

    added RuntimeError module

---
+++ src/system/src/RuntimeError/error.c
@@ -0,0 +1,20 @@
+#include "sac.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+
+void SAC__RUNTIMEERROR_error( int result, const char *message, ...)
+{
+  va_list arg_p;
+
+  fprintf(stderr, "\n\n*** USER runtime error\n");
+  fprintf(stderr, "*** ");
+
+  va_start(arg_p, message);
+  vfprintf(stderr, message, arg_p);
+  va_end(arg_p);
+
+  fprintf(stderr, "\n\n");
+
+  exit(result);
+}
