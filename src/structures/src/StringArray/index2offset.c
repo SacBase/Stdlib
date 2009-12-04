2ce56ff9a0083638332f32a21927372ee6f6e55d
Daniel Rolls <dsr@sac-home.org>
2009-12-04 15:10:12 +0000

    reverted earlier changes

--- src/structures/src/StringArray/index2offset.c
+++ src/structures/src/StringArray/index2offset.c
@@ -7,14 +7,9 @@ int SAC_StringArray_index2offset( int dim, int *idx, int *shp)
 {
   int i, offset;
 
-  if( dim == 0) {
-    offset = 0;
-  }
-  else {
-    offset = idx[0];
-    for( i=1; i< dim; i++) {
-      offset = (offset * shp[i]) + idx[i];
-    }
+  offset = idx[0];
+  for( i=1; i< dim; i++) {
+    offset = (offset * shp[i]) + idx[i];
   }
   return( offset);
 }
