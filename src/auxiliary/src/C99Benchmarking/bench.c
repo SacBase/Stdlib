9bbe827e56da95ded140510403950a9781523843
Daniel Rolls <dsr@sac-home.org>
2010-04-27 00:15:44 +0000

    A unibench-friendly c99 benchmarking module adering to the same interface as the mutc one. For now this just compares two timestamps in seconds and fractions of seconds. I'd like a cuda one next!

---
+++ src/auxiliary/src/C99Benchmarking/bench.c
@@ -0,0 +1,88 @@
+#include "stdarg.h"
+#include "sac.h"
+#include <string.h>
+
+struct bench {                                                   
+  int                num; 
+  char *             name;
+  double             startSec;
+  double             stopSec;
+};                                                                       
+                                                                         
+void benchStart( struct bench *interval, double time) 
+{                                                            
+  interval->startSec = time;
+}                                                                        
+                                                                         
+void benchEnd( struct bench* interval, double time)                                   
+{                                                            
+  interval->stopSec = time;
+}                         
+                                               
+void benchThis( )                                                        
+{                                                                        
+  /* noop*/
+}                                                                        
+
+double benchRes( struct bench* interval)
+{
+  return( interval->stopSec - interval->startSec);
+}
+
+char* benchName( struct bench* interval)
+{
+  char* newName = SAC_MALLOC( strlen( interval->name) + 1);
+  strncpy( newName, interval->name, strlen( interval->name) + 1);
+  return( newName);
+}
+
+int benchNum( struct bench* interval)
+{
+  return( interval->num);
+}
+
+void benchCreate( struct bench** interval)                                                
+{                                                                        
+  /* benchGetInterval actually creates the data structure */
+}     
+ 
+struct bench* benchGetInterval_i( int num)               
+{                                                                        
+  struct bench *interval;
+  interval = (struct bench*)SAC_MALLOC( sizeof( struct bench));
+  interval->num = num;
+  interval->name = (char*)SAC_MALLOC( sizeof( char));
+  *(interval->name) = '\0';
+  return( interval);
+}                                                                        
+
+struct bench* benchGetInterval_s( char *name) 
+{                                                                        
+  struct bench *interval;
+  char* newName;
+  interval = (struct bench*)SAC_MALLOC( sizeof( struct bench));
+  interval->num = -1;
+  newName = SAC_MALLOC( strlen( name) + 1);
+  strncpy( newName, name, strlen( name) + 1);
+  interval->name = newName;
+  return( interval);
+}                                                                        
+
+struct bench* benchGetInterval_si(char * name, int num)
+{                                                                        
+  struct bench *interval;
+  char* newName;
+  interval = (struct bench*)SAC_MALLOC( sizeof( struct bench));
+  interval->num = num;                                    
+  newName = SAC_MALLOC( strlen( name) + 1);
+  strncpy( newName, name, strlen( name) + 1);
+  interval->name = newName;
+  return( interval);
+}                                                                        
+                                                                         
+void benchDestroyInterval( struct bench *interval)                        
+{                                                                        
+  free(interval->name);
+  free(interval);                                                        
+}                            
+                                            
