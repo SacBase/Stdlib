7f0d713ab40b1677ce965345a2d105a9d470b6ef
Bep Rinto <bro@sac-home.org>
2012-03-01 18:11:09 +0000

    fixed three bugs: There was a memory leak, because format was malloced but never freed. The function is supposed to return the next string consisting of non-whitespace characters, but its returned string included whitespace. Leading whitespace wasn't properly discarded.

--- src/stdio/src/File/fscans.c
+++ src/stdio/src/File/fscans.c
@@ -10,26 +10,24 @@
 
 /*****************************************************************/
 
-char* fscans(FILE *stream, int length)
+char* fscans(FILE* stream, int length)
 {
   int success;
-  char *input, *format;
-  
-  input=(char*)SAC_MALLOC(length+1);
-  format=(char*)SAC_MALLOC(32);
-  sprintf(format, "%%%d[^\n]", length);
-  
-  success=fscanf(stream, format, input);
-  
-  if (success!=1)
+  char *input;
+  char format[32];
+
+  input = (char *) SAC_MALLOC(length + 1);
+  snprintf(format, sizeof format, " %%%ds", length);
+
+  success = fscanf(stream, format, input);
+
+  if (success != 1)
   {
-    input[0]=0;
+    input[0] = 0;
   }
-    
-  return(input);
+
+  return (input);
 }
 
 
 /*****************************************************************/
-
-
