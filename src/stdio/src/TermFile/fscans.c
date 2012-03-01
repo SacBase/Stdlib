23a36f2c7620547277d20f0b20b973801143d8a7
Bep Rinto <bro@sac-home.org>
2012-03-01 18:16:30 +0000

    fixed bugs: there was a memory leak since format was malloced but never freed. leading whitespace wasn't properly discarded. A bufferoverflow is prevented by using snprintf instead of sprintf.

--- src/stdio/src/TermFile/fscans.c
+++ src/stdio/src/TermFile/fscans.c
@@ -13,23 +13,21 @@
 char* term_fscans(FILE *stream, int length)
 {
   int success;
-  char *input, *format;
-  
-  input=(char*)SAC_MALLOC(length+1);
-  format=(char*)SAC_MALLOC(32);
-  sprintf(format, "%%%ds", length);
-  
-  success=fscanf(stream, format, input);
-  
-  if (success!=1)
+  char *input;
+  char format[32];
+
+  input = (char *) SAC_MALLOC(length + 1);
+  snprintf(format, sizeof format, " %%%ds", length);
+
+  success = fscanf(stream, format, input);
+
+  if (success != 1)
   {
-    input[0]=0;
+    input[0] = 0;
   }
-    
-  return(input);
+
+  return (input);
 }
 
 
 /*****************************************************************/
-
-
