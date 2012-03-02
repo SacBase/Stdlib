cb4a1a186340b67e2fd55f4e05d665cb4a4634f4
Bep Rinto <bro@sac-home.org>
2012-03-02 08:26:08 +0000

    added a function filesize().

--- src/system/src/FileSystem/testfile.c
+++ src/system/src/FileSystem/testfile.c
@@ -13,21 +13,21 @@
 int SACisdir(int *success, char *name)
 {
   struct stat buf;
-  int result=0;
-  
-  *success=stat(name, &buf);
-  
-  if (*success==-1)
+  int result = 0;
+
+  *success = stat(name, &buf);
+
+  if (*success == -1)
   {
-    *success=errno;
+    *success = errno;
   }
   else
   {
-    *success=-1;
-    result=S_ISDIR(buf.st_mode);
+    *success = -1;
+    result = S_ISDIR(buf.st_mode);
   }
-  
-  return(result);
+
+  return (result);
 }
 
 
@@ -36,50 +36,71 @@ int SACisdir(int *success, char *name)
 int SACisreg(int *success, char *name)
 {
   struct stat buf;
-  int result=0;
-  
-  *success=stat(name, &buf);
-  
-  if (*success==-1)
+  int result = 0;
+
+  *success = stat(name, &buf);
+
+  if (*success == -1)
   {
-    *success=errno;
+    *success = errno;
   }
   else
   {
-    *success=-1;
-    result=S_ISREG(buf.st_mode);
+    *success = -1;
+    result = S_ISREG(buf.st_mode);
   }
-  
-  return(result);
+
+  return (result);
 }
 
 
-  
+
 /*****************************************************************/
 
 int SACislnk(int *success, char *name)
 {
   struct stat buf;
-  int result=0;
-  
-  *success=stat(name, &buf);
-  
-  if (*success==-1)
+  int result = 0;
+
+  *success = stat(name, &buf);
+
+  if (*success == -1)
   {
-    *success=errno;
+    *success = errno;
   }
   else
   {
-    *success=-1;
-    result=S_ISLNK(buf.st_mode);
+    *success = -1;
+    result = S_ISLNK(buf.st_mode);
   }
-  
-  return(result);
+
+  return (result);
 }
 
 
 
 /*****************************************************************/
 
+unsigned long long SACfilesize(int *success, char *name)
+{
+  struct stat buf;
+  unsigned long long result = 0;
+
+  *success = stat(name, &buf);
+
+  if (*success == -1)
+  {
+    *success = errno;
+  }
+  else
+  {
+    *success = -1;
+    result = (unsigned long long) buf.st_size;
+  }
+
+  return (result);
+}
 
 
+
+/*****************************************************************/
