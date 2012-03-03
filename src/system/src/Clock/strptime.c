44a5b9b7a6357a7dccbfa54570aa64704d375d74
Bep Rinto <bro@sac-home.org>
2012-03-03 07:52:36 +0000

    The error return value for strptime wasn't checked. Fixed source and updated comments.

--- src/system/src/Clock/strptime.c
+++ src/system/src/Clock/strptime.c
@@ -13,6 +13,9 @@ extern char *strptime(const char *, const char *, struct tm *);
  * Unfortunately, the function strptime() is not declared in time.h !
  * Fortunately, the standard library libc.a contains an implementation
  * of  strptime().
+ *
+ * bro: Actually, time.h does have a prototype for strptime(), but
+ * then _XOPEN_SOURCE must be defined before including time.h.
  */
 
 
@@ -20,25 +23,33 @@ extern char *strptime(const char *, const char *, struct tm *);
 /******************************************************************/
 
 
-time_t *SACstrptime(string *result, string s, string format)
+time_t *SACstrptime(string * result, string s, string format)
 {
   struct tm tt;
   string remain;
   time_t *t;
-  
-  remain=strptime(s, format, &tt);
-  
-  *result=(string)SAC_MALLOC(strlen(remain)+1);
-  strcpy(*result, remain);
-  
-  t=(time_t*)SAC_MALLOC(sizeof(time_t));
-  *t=mktime(&tt);
-  
-  return(t);
+
+  t = (time_t *) SAC_MALLOC(sizeof(time_t));
+
+  memset(&tt, 0, sizeof tt);
+  remain = strptime(s, format, &tt);
+
+  /* strptime() may return NULL if it fails to match all of the format string.
+   * In that case an error occurred and the contents of tt is undefined.
+   */
+  if (remain == NULL) {
+    *t = 0;
+    *result = (string) SAC_MALLOC(1);
+    **result = '\0';
+  } else {
+    *t = mktime(&tt);
+    *result = (string) SAC_MALLOC(strlen(remain) + 1);
+    strcpy(*result, remain);
+  }
+
+  return (t);
 }
 
 
 
 /******************************************************************/
-
-
