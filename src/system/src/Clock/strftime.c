458f2e5fca2e9cea45946f9f54061b0e7210d2cd
Bep Rinto <bro@sac-home.org>
2012-03-03 08:12:26 +0000

    strftime didn't check any of the returned values.

--- src/system/src/Clock/strftime.c
+++ src/system/src/Clock/strftime.c
@@ -8,22 +8,24 @@
 
 /******************************************************************/
 
-string SACstrftime(int len, string format, time_t *t)
+string SACstrftime(int len, string format, time_t* t)
 {
   string result;
   struct tm *tt;
-  
-  tt=localtime(t);
-  
-  result=(string)SAC_MALLOC(len+1);
-  
-  strftime(result, len, format, tt);
-  
-  return(result);
+
+  result = (string) SAC_MALLOC(len + 1);
+  result[0] = '\0';
+
+  tt = localtime(t);
+  if (tt != NULL) {
+      if (strftime(result, len, format, tt) == 0) {
+          result[0] = '\0';
+      }
+  }
+
+  return (result);
 }
 
 
 
 /******************************************************************/
-
-
