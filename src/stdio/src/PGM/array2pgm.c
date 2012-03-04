cd39fa414b04389d4dff8b698bfe3be8afba2bfc
Bep Rinto <bro@sac-home.org>
2012-03-04 15:01:56 +0000

    Rewrote the PGM library as it was buggy, non-functional and incomplete.

--- src/stdio/src/PGM/array2pgm.c
+++ src/stdio/src/PGM/array2pgm.c
@@ -1,54 +1,149 @@
+/*
+ * Implement writing PGM images for SAC.
+ * Support saving in either binary or ASCII representation.
+ * Support both 8-bit and 16-bit pixel values.
+ * Detect when pixel data which lie outside of the valid range.
+ * Support large PGM data efficiently.
+ * Detect and report all errors.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <unistd.h>
-
 #include "sac.h"
 
-#define array_nt  (array, T_OLD((AKD, (NHD, (NUQ, )))))
+typedef char *string;
 
-void SAC_PGM_array2pgm( FILE* fp,
-                        SAC_ND_PARAM_in_nodesc( array_nt, int),
-                        const int shape[2],
-                        const bool binary)
+#define PGM_MAX_VAL      65535
+#define PGM_MIN_VAL      0
+#define PGM_MAX_BYTE_VAL 255
+#define PGM_LINE         70
+
+#define array_nt  (array, T_OLD((AUD, (NHD, (NUQ, )))))
+#define string_nt (str,   T_OLD((SCL, (HID, (NUQ, )))))
+
+#if defined(_IO_putc_unlocked)
+#define myput(c,f)      _IO_putc_unlocked(c,f)
+#else
+#define myput(c,f)      putc(c,f)
+#endif
+
+void write_pgm(SAC_ND_PARAM_in_nodesc(array_nt, int),
+               const int shape[2],
+               const bool binary, SAC_ND_PARAM_in_nodesc(string_nt, string))
 {
-  const int w = shape[1];
-  const int h = shape[0];
+  const char *filename = NT_NAME(string_nt);
+  const int height = shape[0];
+  const int width = shape[1];
+  int *restrict int_array = SAC_ND_A_FIELD(array_nt);
+  const int pixelcount = height * width;
+  int min = PGM_MAX_VAL;
+  int max = PGM_MIN_VAL;
+  int maxval;
+  FILE *fp;
 
-  /*
-   * ASCII output
-   */
-  if ( binary == false) {
-    fprintf(fp, "P2\n");
+  for (int i = 0; i < pixelcount; ++i)
+  {
+    if (min > int_array[i])
+    {
+      min = int_array[i];
+    }
+    if (max < int_array[i])
+    {
+      max = int_array[i];
+    }
+  }
+  if (min < PGM_MIN_VAL)
+  {
+    SAC_RuntimeError("write_pgm: Image minimum %d is less than %d for %s\n",
+                     min, PGM_MIN_VAL, filename);
+  }
+  if (max > PGM_MAX_VAL)
+  {
+    SAC_RuntimeError("write_pgm: Image maximum %d is more than %d for %s\n",
+                     max, PGM_MAX_VAL, filename);
+  }
 
-    fprintf(fp, "%d %d\n", w, h);
-    fprintf(fp, "255\n");
+  maxval = (max <= PGM_MAX_BYTE_VAL) ? PGM_MAX_BYTE_VAL : PGM_MAX_VAL;
 
-    for(int i = 0; i < h; i++) {
-      for (int j = 0; j < w; j++) {
-        fprintf(fp, "%d", SAC_ND_A_FIELD(array_nt)[i*w+j]);
+  fp = fopen(filename, "wb");
+  if (fp == NULL)
+  {
+    SAC_RuntimeError("write_pgm: Could not open %s for writing: %s",
+                     filename, strerror(errno));
+  }
 
-        if (j != w-1) {
-          fprintf(fp, " ");
-        }
+  fprintf(fp, "P%c\n%d %d\n%d\n", binary ? '5' : '2', width, height, maxval);
+
+  if (binary)
+  {
+    if (maxval <= PGM_MAX_BYTE_VAL)
+    {
+      for (int i = 0; i < pixelcount; ++i)
+      {
+        myput(int_array[i], fp);
       }
-      fprintf(fp, "\n");
     }
+    else
+    {
+      for (int i = 0; i < pixelcount; ++i)
+      {
+        unsigned char a = (unsigned char) (int_array[i] >> 8);
+        unsigned char b = (unsigned char) (int_array[i] & 0xFF);
 
-  }
-  /*
-   * Binary output
-   */
-  else {
-    fprintf(fp, "P5\n");
-
-    fprintf(fp, "%d %d\n", w, h);
-    fprintf(fp, "255\n");
-
-    for(int i = 0; i < w * h; i++) {
-      fprintf(fp, "%c", SAC_ND_A_FIELD(array_nt)[i]);
+        myput(a, fp);
+        myput(b, fp);
+      }
     }
   }
+  else
+  {
+    int col = 0;
 
+    for (int i = 0; i < pixelcount; ++i)
+    {
+      unsigned char rep[20];
+      int dig = 0;
+      int num = int_array[i];
+
+      while (num >= 10)
+      {
+        rep[dig++] = '0' + (num % 10);
+        num /= 10;
+      }
+      rep[dig++] = '0' + num;
+      if (col + dig >= PGM_LINE)
+      {
+        myput('\n', fp);
+        col = 0;
+      }
+      else if (i)
+      {
+        myput(' ', fp);
+        ++col;
+      }
+      col += dig;
+      while (--dig >= 0)
+      {
+        myput(rep[dig], fp);
+      }
+    }
+    if (col)
+    {
+      myput('\n', fp);
+    }
+  }
+
+  if (ferror(fp))
+  {
+    SAC_RuntimeError("write_pgm: Errors while writing to %s: %s",
+                     filename, strerror(errno));
+  }
+
+  fclose(fp);
 }
-         
+
--- src/stdio/src/PGM/pgm2array.c
+++ src/stdio/src/PGM/pgm2array.c
@@ -1,135 +1,230 @@
+/*
+ * Implement reading of PGM images for SAC.
+ * Support all details of the PGM specification:
+ *  - both binary and ASCII representation PGM images.
+ *  - both 8-bit and 16-bit pixel values.
+ *  - comments in PGM files, even in weird places.
+ * Support large PGM data efficiently.
+ * Detect all invalid data conditions such as image
+ * dimensions which exceed a 32-bit address space.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
 #include "sac.h"
 
-typedef char* string;
+static int *read_pgm_int(char *fn, int *width_ptr, int *height_ptr,
+                         int *maxval_ptr);
 
-#define array_nt  (array, T_OLD((AKD, (NHD, (NUQ, )))))
-#define ret_nt    (ret,   T_OLD((AKD, (NHD, (NUQ, )))))
+#define array_nt (array, T_OLD((AUD, (NHD, (NUQ, )))))
+#define ret_nt   (ret,   T_OLD((AUD, (NHD, (NUQ, )))))
 
-#define MAXLINE 80
-
-/*
- * Read a pgm (ascii or binary) image.
- */
-void SAC_PGM_pgm2array( SAC_ND_PARAM_out( array_nt, int),
-                        FILE *fp)
+void read_pgm(SAC_ND_PARAM_out(array_nt, int), int *maxval, char *fn)
 {
-  char line[MAXLINE];
-  int w;
-  int h;
-  int max;
-  int *data;
-  unsigned int c;
-  bool binary;
-  bool comment;
+  SAC_ND_DECL__DATA(ret_nt, int,) SAC_ND_DECL__DESC(ret_nt,) int width = 0;
+  int height = 0;
+  int *intarray = read_pgm_int(fn, &width, &height, maxval);
+  int num_dims = 2;
 
-  /*
-   * Check for a valid header
-   */
-  if (fgets(line, MAXLINE, fp) == NULL) {
-    SAC_RuntimeError( "Unexpected error when reading file");
-  }
-  if ( (line[0] != 'P' && line[1] != '2') || (line[0] != 'P' && line[1] != '5') ) {
-    SAC_RuntimeError( "Not reading a pgm file!");
-  }
+  int SAC_ND_A_MIRROR_DIM(ret_nt) = num_dims;
 
-  /*
-   * Check if we are reading an ascii or binary file
-   */
-  if (line[1] == '2') {
-    binary = false;
-  } else {
-    binary = true;
-  }
+  SAC_ND_ALLOC__DESC(ret_nt, num_dims)
+    SAC_ND_SET__RC(ret_nt, 1) SAC_ND_A_DESC_SHAPE(ret_nt, 0) = height;
+  SAC_ND_A_DESC_SHAPE(ret_nt, 1) = width;
+  SAC_ND_A_DESC_SIZE(ret_nt) = width * height;
+  SAC_ND_A_FIELD(ret_nt) = intarray;
+SAC_ND_RET_out(array_nt, ret_nt)}
 
-  /*
-   * Check for comment (and ignore it)
-   */
-  while(true) {
-    if (fgets(line, MAXLINE, fp) == NULL) {
-      SAC_RuntimeError( "Unexpected error when reading file");
-    }
+#undef array_nt
+#undef ret_nt
 
-    /*
-     * Check for a comment line
-     */
-    if (line[0] == '#') {
-      comment = true;
+#include <string.h>
+#include <errno.h>
+#include <ctype.h>
+#include <limits.h>
 
-      while(comment == true) {
-        for(int i = 0; i < MAXLINE; i++) {
-          if (line[i] == '\n') {
-            comment = false;
-            break;
-          }
+#define MAX_PGM_BYTE_VAL        255
+#define MAX_PGM_VAL             65535
+
+#if defined(_IO_getc_unlocked)
+#define myget(f)      _IO_getc_unlocked(f)
+#else
+#define myget(f)      getc(f)
+#endif
+
+/* Read the next character from the input stream 'fp'.
+ */
+#define next()        do { c = myget(fp); } while (0)
+
+/* Ignore comments.
+ * Skip the rest of the line and ignore more comments until 
+ * the first character of the next line is not a comment character.
+ */
+#define comment()                                          \
+        do {                                               \
+          do {                                             \
+            next();                                        \
+          } while (c != '\n' && c != '\r' && c != EOF);    \
+          if (c == '\r') { next(); }                       \
+          if (c == '\n') { next(); }                       \
+        } while (c == '#')
+
+
+/* Expect to find a whitespace character or a comment delimiter.
+ * Skip all whitespace characters and comments.
+ */
+#define skip()                                 \
+        if (c != '#' && !isspace(c)) {         \
+          if (c == EOF) {                      \
+            goto eof;                          \
+          } else {                             \
+            goto bad;                          \
+          }                                    \
+        } else do {                            \
+          if (c == '#') {                      \
+            comment();                         \
+          } else {                             \
+            next();                            \
+          }                                    \
+        } while (c == '#' || isspace(c))
+
+/* Expect to find a digit.
+ * Convert this and subsequent digits to an unsigned number in 'n'.
+ */
+#define num(n)                                 \
+        if (!isdigit(c)) {                     \
+          goto bad;                            \
+        } else {                               \
+          n = c - '0';                         \
+          next();                              \
+          while (isdigit(c)) {                 \
+            n = n * 10 + (c - '0');            \
+            next();                            \
+          }                                    \
         }
 
-        if (comment == true) {
-          fgets(line, MAXLINE, fp);
-        }
+
+static int *read_pgm_int(char *fn,
+                         int *width_ptr, int *height_ptr, int *maxval_ptr)
+{
+  int *data = NULL;
+  FILE *fp;
+  size_t width = 0, height = 0, pixelcount = 0;
+  unsigned maxval = 0, val = 0;
+  int hdr[2];
+  /* 'c' is used by the macros to store the next input character. */
+  int c;
+
+  fp = fopen(fn, "rb");
+  if (fp == NULL)
+  {
+    SAC_RuntimeError("read_pgm: Cannot open \"%s\": %s", fn, strerror(errno));
+    return NULL;
+  }
+
+  hdr[0] = myget(fp);
+  hdr[1] = myget(fp);
+  if (hdr[0] != 'P' || (hdr[1] != '2' && hdr[1] != '5'))
+  {
+    SAC_RuntimeError("read_pgm: File \"%s\" is not in PGM format", fn);
+    goto done;
+  }
+
+  next();
+  skip();
+  num(width);
+  skip();
+  num(height);
+  skip();
+  num(maxval);
+
+  if (width == 0 || height == 0 || maxval > MAX_PGM_VAL)
+  {
+    SAC_RuntimeError("read_pgm: File \"%s\" has invalid PGM header", fn);
+    goto done;
+  }
+
+  pixelcount = width * height;
+  if (width > INT_MAX || height > INT_MAX ||
+      pixelcount > INT_MAX || pixelcount / width != height)
+  {
+    SAC_RuntimeError(
+      "read_pgm: Image dimensions of \"%s\" (%u * %u = %u) exceed INT_MAX",
+      fn, width, height, pixelcount);
+    goto done;
+  }
+
+  data = SAC_MALLOC(pixelcount * sizeof(int));
+
+  if (hdr[1] == '2')
+  {
+    for (size_t i = 0; i < pixelcount; ++i)
+    {
+      skip();
+      num(val);
+      data[i] = (int) val;
+    }
+  }
+  else if (hdr[1] == '5')
+  {
+    if (c == '#') {
+      comment();
+    }
+    if (!isspace(c)) {
+      if (c == EOF) {
+        goto eof;
+      } else {
+        goto bad;
       }
-    } else {
-      break;
+    }
+    if (maxval <= MAX_PGM_BYTE_VAL)
+    {
+      for (size_t i = 0; i < pixelcount; ++i)
+      {
+        data[i] = myget(fp);
+      }
+    }
+    else
+    {
+      for (size_t i = 0; i < pixelcount; ++i)
+      {
+        int b1 = myget(fp);
+        int b2 = myget(fp);
+
+        data[i] = ((b1 << 8) | b2);
+      }
     }
   }
 
-  /*
-   * Get the dimensions of the image
-   * and allocate an array of sufficient size
-   */
-  if (sscanf(line, "%d %d", &w, &h) != 2) {
-    SAC_RuntimeError( "Unexpected error when reading file");
+  if (ferror(fp)) {
+    goto err;
   }
-  data = malloc(sizeof(int) * w * h);
-  if (data == NULL) {
-    SAC_RuntimeError( "Could not allocate data for image array");
+  if (feof(fp) && (hdr[1] != '2' || val > MAX_PGM_VAL)) {
+    goto eof;
   }
 
-  /*
-   * Read out the maximum gray value
-   */
-  if (fgets(line, MAXLINE, fp) == NULL) {
-    SAC_RuntimeError( "Unexpected error when reading file");
-  }
-  if (sscanf(line, "%d", &max) != 1) {
-    SAC_RuntimeError( "Unexpected error when reading file");
+done:
+  *width_ptr = width;
+  *height_ptr = height;
+  *maxval_ptr = maxval;
+  return data;
+
+bad:
+  if (c != EOF && !ferror(fp) && !feof(fp)) {
+    SAC_RuntimeError("Invalid PGM data in \"%s\".", fn);
+    goto done;
   }
 
-  /*
-   * Readout the image
-   */
-  if (binary == true) {
-    for (int i = 0; i < h*w; i++) {
-      c = 0;
-      if(fread(&c, 1, 1, fp) != 1) {
-        SAC_RuntimeError( "Unexpected end of file" );
-      }
-      data[i] = c * (255/max);
-    }
-  } else {
-    for (int i = 0; i < h*w; i++) {
-      if( fscanf(fp, "%u", &c) != 1) {
-        SAC_RuntimeError( "Unexpected end of file");
-      }
-      data[i] = c * (255/max);
-    }
+err:
+  if (ferror(fp)) {
+    SAC_RuntimeError("read_pgm: Errors while reading PGM data from \"%s\".",
+                     fn);
+    goto done;
   }
 
-  /*
-   * Create the SAC resturn array & descriptor
-   */
-  SAC_ND_DECL__DATA( ret_nt, int, )
-  SAC_ND_DECL__DESC( ret_nt, )
-  int SAC_ND_A_MIRROR_DIM( ret_nt) = 2;
-  SAC_ND_ALLOC__DESC( ret_nt, dims)
-  SAC_ND_SET__RC( ret_nt, 1)
-  SAC_ND_A_DESC_SHAPE( ret_nt, 0) = h;
-  SAC_ND_A_DESC_SHAPE( ret_nt, 1) = w;
-  SAC_ND_A_FIELD( ret_nt) = data;
-  SAC_ND_RET_out( array_nt, ret_nt)
+eof:
+  SAC_RuntimeError(
+    "read_pgm: Premature end-of-file while reading PGM data from \"%s\".", fn);
+  goto done;
 }
-         
+
