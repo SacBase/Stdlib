047f6ac367ee79983b795c27d6818212413171b0
Raphael 'kena' Poss <knz@thaumogen.net>
2013-10-01 18:35:50 +0200

    Remove use of obsolete P_tmpdir.

--- src/system/src/FileSystem/pltmp.c
+++ src/system/src/FileSystem/pltmp.c
@@ -12,11 +12,10 @@ char *SACPtmpdir()
 {
   char *result;
 
-/* FIXME: CYGWINHACK temporarily broken to get the stdlib compiling under cygwin */
-//  char *P_tmpdir = "";
+  // P_tmpdir is an obsolete feature. It should really not be used at all.
 
-  result=(char *)SAC_MALLOC(strlen(P_tmpdir)+1);
-  strcpy(result, P_tmpdir);
+  result=(char *)SAC_MALLOC(4);
+  strcpy(result, "/tmp");
   
   return(result);
 }
