49a92672aab056bbcd82edd7bc2ea2cb87ec97fe
Hans-Nikolai Viessmann <hansthespass@googlemail.com>
2015-09-20 10:44:48 +0100

    Minor change to FileSystem module

--- src/system/src/FileSystem/pltmp.c
+++ src/system/src/FileSystem/pltmp.c
@@ -12,13 +12,6 @@ char *SACPtmpdir()
 {
   char *result;
 
-/* FIXME: CYGWINHACK temporarily broken to get the stdlib compiling under cygwin */
-  result=(char *)SAC_MALLOC(strlen(P_tmpdir)+1);
-  strcpy(result, P_tmpdir);
-
-/* code from a commit, which is better though is not known by me HNV */
-  // P_tmpdir is an obsolete feature. It should really not be used at all.
-
   result=(char *)SAC_MALLOC(4);
   strcpy(result, "/tmp");
   
