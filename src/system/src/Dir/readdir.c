bbd6dceed52edffeddd7e6487f76177fe1ad947d
Sven-Bodo Scholz <sbs@sac-home.org>
2017-12-05 16:08:12 +0000

    replaced the use of readdir_r by readdir
    However, left the old version behind a macro just in case we encounter a system that does support readdir_r instead of readdir.

--- src/system/src/Dir/readdir.c
+++ src/system/src/Dir/readdir.c
@@ -6,25 +6,40 @@
 
 #include "Dir.h"
 
-
+/*
+ * SBS: I assume all systems do have readdir
+ * If they turn out not to, we may have to preset the macro
+ * HAVE_READDIR through cmake...
+ */
+#define HAVE_READDIR 1
 
 /*****************************************************************/
 
 string SACreaddir( DIR* stream)
 {
+
+#if HAVE_READDIR
+#else // HAVE_READDIR
   /* some old OSes don't allocate enough room for entry.d_name. */
   struct bigenough {
     struct dirent entry;
     char name[1024];
   } big;
-  struct dirent* entptr;
   int r;
+#endif // HAVE_READDIR
+
+  struct dirent* entptr;
   string name = NULL;
 
   while (name == NULL) {
+#if HAVE_READDIR
+    entptr = readdir( stream);
+    if (entptr != NULL) {
+#else // HAVE_READDIR
     entptr = NULL;
     r = readdir_r( stream, &big.entry, &entptr);
     if (r == 0 && entptr != NULL) {
+#endif // HAVE_READDIR
       char *s = entptr->d_name;
       /* ignore "." and ".." */
       if (*s == '.' && (s[1] == '\0' || (s[1] == '.' && s[2] == '\0'))) {
@@ -40,6 +55,5 @@ string SACreaddir( DIR* stream)
 
   return( name);
 }
-
 
 /*****************************************************************/
