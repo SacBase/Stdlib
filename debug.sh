2cdac35c296f1cdd8721a4272cdf144aa68fab20
thomas <thomas.koopman@ru.nl>
2023-07-07 11:45:56 +0200

    Debug script also formats the .i files now

--- debug.sh
+++ debug.sh
@@ -15,8 +15,37 @@ mkdir -p source_files
 # Copy the .c files from tmp to source_files
 grep -E -oh "/tmp/[^/]*/[^.]*\.c" "$1" | xargs cp -t ./source_files
 
-# Run the preprocessor, storing the output in source_files
-sed -i -E "s/\/tmp\/[^/]*\/([^.]*\.i)/\.\/source_files\/\1/g" "$1"
+# Create the command to run the preprocessor storing the output in source_files
+sed -i -E 's/\/tmp\/[^/]*\/([^.]*\.i)/\.\/source_files\/\1/g' "$1"
 
-# Make sure gdb uses the .i file and not .c
+# Run the preprocessor command
+grep "\-E" "$1" | sh
+
+# Format the .i files
+format_in_place()
+{
+    temp=$(mktemp)
+
+    grep -v "#" "$1" | nuke_semi | clang-format > "$temp"
+    mv "$temp" "$1"
+}
+
+for file in ./source_files/*
+do
+    format_in_place "$file"
+done
+
+# Delete the preprocessing commands from the .sac2c-file
+sed -i '/-E/d' "$1"
+
+# Create the command to make sure gdb uses the .i file and not .c
 sed -i 's/\/usr\/bin\/cc/\/usr\/bin\/cc\ -no-integrated-cpp -P/g' "$1"
+
+# Compile the objects files
+grep "\-c" "$1" | sh
+
+# Delete those commands from the .sac2c-file
+sed -i '/-c\ /d' "$1"
+
+# Run the remaining commands (making directories and linking)
+sh < "$1"
