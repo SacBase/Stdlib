56dad8fafbfac5eec632e9161064260e78748bf9
Jordy Aaldering <aalderingjordy@gmail.com>
2025-05-09 09:37:04 +0200

    posix guarantees that CLOCK_REALTIME exists; no need to check for it

--- src/system/src/RTClock/rtclock.c
+++ src/system/src/RTClock/rtclock.c
@@ -1,63 +1,47 @@
-/*
- * $Id$
+/******************************************************************************
  *
- * Description:
+ * @file rtclock.c
  *
- * This module implements the functionally sound integration of the real
- * time clock into the world of SAC.
- */
-
+ * @brief This module implements the functionally sound integration of the
+ * real time clock into the world of SAC.
+ *
+ ******************************************************************************/
 #include <time.h>
-#include "libsac/essentials/message.h"
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#ifdef HAVE_GETTIME_REALTIME
-#include <sys/times.h>
-#elif HAVE_MACH_CLOCK_GET_TIME
+#ifdef __MACH__
 #include <mach/clock.h>
 #include <mach/mach.h>
 #endif
 
-
-void *SAC_RTClock_createTheRTClock( void)
+void *SAC_RTClock_createTheRTClock(void)
 {
-  return((void*)0);
+    return((void*)0);
 }
 
-void SAC_RTClock_touch( void *rtclock)
+void SAC_RTClock_touch(void *rtclock)
 {
+    /* noop */
 }
 
-void SAC_RTClock_gettime( long *sec, long *nano)
+void SAC_RTClock_gettime(long *sec, long *nsec)
 {
-  struct timespec result;
+    struct timespec result;
+    result.tv_sec = 0;
+    result.tv_nsec = 0;
 
-  result.tv_sec = 0;
-  result.tv_nsec = 0;
-
-#ifdef HAVE_GETTIME_REALTIME
-
-  clock_gettime( CLOCK_REALTIME, &result);
-
-#elif HAVE_MACH_CLOCK_GET_TIME
-  clock_serv_t cclock;
-  mach_timespec_t mts;
-  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-  clock_get_time(cclock, &mts);
-  mach_port_deallocate(mach_task_self(), cclock);
-  result.tv_sec = mts.tv_sec;
-  result.tv_nsec = mts.tv_nsec;
-
+#ifdef __MACH__
+    // OS X does not have clock_gettime, use clock_get_time
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    result.tv_sec = mts.tv_sec;
+    result.tv_nsec = mts.tv_nsec;
 #else
-  SAC_RuntimeError( "When the stdlib was compiled for this architecture"
-                      " neither clock_gettime( CLOCK_REALTIME, x) "
-                      " nor host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, x)"
-                      " were found");
+    clock_gettime(CLOCK_REALTIME, &result);
 #endif
 
-  *sec = result.tv_sec;
-  *nano = result.tv_nsec;
+    *sec = result.tv_sec;
+    *nsec = result.tv_nsec;
 }
--- src/system/src/RTimer/rtimer.c
+++ src/system/src/RTimer/rtimer.c
@@ -12,43 +12,28 @@
  * step towards automatically inserting timer calls into the code by the compiler,
  * where we need to take recursive functions into account.
  */
-
-
-
 #include <time.h>
 #include "sac.h"
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#ifdef HAVE_GETTIME_REALTIME
-#include <sys/times.h>
-#elif HAVE_MACH_CLOCK_GET_TIME
+#ifdef __MACH__
 #include <mach/clock.h>
 #include <mach/mach.h>
 #endif
 
-
 struct rtimer
 {
-  struct timespec elapsed;
-  struct timespec started;
-  int instance;
+    struct timespec elapsed;
+    struct timespec started;
+    int instance;
 };
 
-
-void current_utc_time(struct timespec *ts) {
-
+void current_utc_time(struct timespec *ts)
+{
     ts->tv_sec = 0;
     ts->tv_nsec = 0;
 
-#ifdef HAVE_GETTIME_REALTIME
-
-    clock_gettime( CLOCK_REALTIME, ts);
-
-#elif HAVE_MACH_CLOCK_GET_TIME
-
+#ifdef __MACH__
+    // OS X does not have clock_gettime, use clock_get_time
     clock_serv_t cclock;
     mach_timespec_t mts;
     host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
@@ -56,86 +41,74 @@ void current_utc_time(struct timespec *ts) {
     mach_port_deallocate(mach_task_self(), cclock);
     ts->tv_sec = mts.tv_sec;
     ts->tv_nsec = mts.tv_nsec;
-
 #else
-    SAC_RuntimeError( "When the stdlib was compiled for this architecture"
-                      " neither clock_gettime( CLOCK_REALTIME, x) "
-                      " nor host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, x)"
-                      " were found");
+    clock_gettime(CLOCK_REALTIME, ts);
 #endif
-
 }
 
-
-
-void SAC_RTimer_createRTimer( struct rtimer **ts)
+void SAC_RTimer_createRTimer(struct rtimer **ts)
 {
-  *ts = (struct rtimer *) SAC_MALLOC( sizeof( struct rtimer));
-  (*ts)->elapsed.tv_sec = 0;
-  (*ts)->elapsed.tv_nsec = 0;
-  (*ts)->started.tv_sec = 0;
-  (*ts)->started.tv_nsec = 0;
-  (*ts)->instance = 0;
+    *ts = (struct rtimer *)SAC_MALLOC(sizeof(struct rtimer));
+    (*ts)->elapsed.tv_sec = 0;
+    (*ts)->elapsed.tv_nsec = 0;
+    (*ts)->started.tv_sec = 0;
+    (*ts)->started.tv_nsec = 0;
+    (*ts)->instance = 0;
 }
 
-void SAC_RTimer_destroyRTimer( struct rtimer *ts)
+void SAC_RTimer_destroyRTimer(struct rtimer *ts)
 {
-  SAC_FREE( ts);
+    SAC_FREE(ts);
 }
 
-void SAC_RTimer_startRTimer( struct rtimer *timer)
+void SAC_RTimer_startRTimer(struct rtimer *timer)
 {
-  if (timer->instance == 0) {
-    current_utc_time(&(timer->started));
-    timer->instance += 1;
-  }
+    if (timer->instance == 0) {
+        current_utc_time(&(timer->started));
+        timer->instance += 1;
+    }
 }
 
 void SAC_RTimer_stopRTimer( struct rtimer *timer)
 {
-  struct timespec now;
-  
-  if (timer->instance == 1) {
-    current_utc_time(&now);
-    if (now.tv_nsec > timer->started.tv_nsec) {
-      timer->elapsed.tv_sec += now.tv_sec - timer->started.tv_sec;
-      timer->elapsed.tv_nsec += now.tv_nsec - timer->started.tv_nsec;
+    struct timespec now;
+
+    if (timer->instance == 1) {
+        current_utc_time(&now);
+        if (now.tv_nsec > timer->started.tv_nsec) {
+            timer->elapsed.tv_sec += now.tv_sec - timer->started.tv_sec;
+            timer->elapsed.tv_nsec += now.tv_nsec - timer->started.tv_nsec;
+        }
+        else {
+            timer->elapsed.tv_sec += (now.tv_sec - timer->started.tv_sec) - 1;
+            timer->elapsed.tv_nsec += 1000000000L - timer->started.tv_nsec + now.tv_nsec;
+        }
+
+        timer->instance = 0;
     }
-    else {
-      timer->elapsed.tv_sec += (now.tv_sec - timer->started.tv_sec) - 1;
-      timer->elapsed.tv_nsec += 1000000000L - timer->started.tv_nsec + now.tv_nsec;
+    else if (timer->instance > 1) {
+        timer->instance -= 1;
     }
-    timer->instance = 0;
-  }
-  else if (timer->instance > 1) {
-    timer->instance -= 1;
-  }
 }
 
 void SAC_RTimer_resetRTimer( struct rtimer *timer)
 {
-  if (timer->instance == 0) {
-    timer->elapsed.tv_sec = 0;
-    timer->elapsed.tv_nsec = 0;
-  }
+    if (timer->instance == 0) {
+        timer->elapsed.tv_sec = 0;
+        timer->elapsed.tv_nsec = 0;
+    }
 }
 
-
-void SAC_RTimer_getRTimerInts( struct rtimer *timer, int *sec, int *nsec)
+void SAC_RTimer_getRTimerInts(struct rtimer *timer, int *sec, int *nsec)
 {
-  *sec = (int) (timer->elapsed.tv_sec);
-  *nsec = (int) (timer->elapsed.tv_nsec);
+  *sec = (int)(timer->elapsed.tv_sec);
+  *nsec = (int)(timer->elapsed.tv_nsec);
 }
 
-double SAC_RTimer_getRTimerDbl( struct rtimer *timer)
+double SAC_RTimer_getRTimerDbl(struct rtimer *timer)
 {
-  double res;
-  
-  res = ((double) timer->elapsed.tv_sec) + ((double) timer->elapsed.tv_nsec) / 1000000000.0;
-  
-  return res;
+    double sec = (double)timer->elapsed.tv_sec;
+    double nsec = (double)timer->elapsed.tv_nsec;
+    return sec + nsec / 1000000000.0;
 }
 
-
-  
-  
