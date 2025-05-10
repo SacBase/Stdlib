412d530e0d542be422fe4a60c6d996db17620618
Jordy Aaldering <aalderingjordy@gmail.com>
2025-05-10 10:14:53 +0200

    link against math.h functions directly

--- src/numerical/src/Math/isinf.c
+++
@@ -1,9 +0,0 @@
-/**
- * Proper C function wrappers around MACROS.
- */
-#include <math.h>
-
-int SAC_MATH_isinf( double X)
-{
-  return( isinf( X));
-}
--- src/numerical/src/Math/isnan.c
+++
@@ -1,9 +0,0 @@
-/**
- * Proper C function wrappers around MACROS.
- */
-#include <math.h>
-
-int SAC_MATH_isnan( double X)
-{
-  return( isnan( X));
-}
