3e72d50f0242be5433497e3d0555e11812d06484
Jordy Aaldering <aalderingjordy@gmail.com>
2026-01-21 18:39:45 +0100

    just a cast needed for runtimerror

--- src/system/src/RuntimeError/error.c
+++ src/system/src/RuntimeError/error.c
@@ -1,21 +1,22 @@
 #include "sac.h"
+#include "sacinterface.h"
 
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 
-void SAC__RUNTIMEERROR_error( int result, const char *message, ...)
+void SAC__RUNTIMEERROR_error(sac_int status, const char *msg, ...)
 {
-  va_list arg_p;
+    va_list arg_p;
 
-  fprintf(stderr, "\n\n*** USER runtime error\n");
-  fprintf(stderr, "*** ");
+    fprintf(stderr, "\n\n*** USER runtime error\n");
+    fprintf(stderr, "*** ");
 
-  va_start(arg_p, message);
-  vfprintf(stderr, message, arg_p);
-  va_end(arg_p);
+    va_start(arg_p, msg);
+    vfprintf(stderr, msg, arg_p);
+    va_end(arg_p);
 
-  fprintf(stderr, "\n\n");
+    fprintf(stderr, "\n\n");
 
-  exit(result);
+    exit((int)status);
 }
