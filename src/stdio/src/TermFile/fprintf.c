ce74a4407e3b764c71620eeec0b65869c2d4328b
Jordy Aaldering <aalderingjordy@gmail.com>
2026-01-22 16:16:11 +0100

    it is a bit of a pain for users if printf has a return type

--- src/stdio/src/TermFile/fprintf.c
+++ src/stdio/src/TermFile/fprintf.c
@@ -4,13 +4,11 @@
 
 #include "TermFile.h"
 
-sac_int SACfprintf_TF(FILE *stream, char *format, ...)
+void SACfprintf_TF(FILE *stream, char *format, ...)
 {
     va_list args;
 
     va_start(args, format);
-    sac_int ret = (sac_int)vfprintf( stream, format, args);
+    vfprintf( stream, format, args);
     va_end(args);
-
-    return ret;
 }
--- src/stdio/src/TermFile/printf.c
+++ src/stdio/src/TermFile/printf.c
@@ -4,13 +4,11 @@
 
 #include "TermFile.h"
 
-sac_int SACprintf_TF(char *format, ...)
+void SACprintf_TF(char *format, ...)
 {
     va_list args;
 
     va_start(args, format);
-    sac_int ret = (sac_int)vprintf(format, args);
+    vprintf(format, args);
     va_end(args);
-
-    return ret;
 }
