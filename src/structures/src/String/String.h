6027c053609a28b251b26405176f5c8876cec6a8
Niek Janssen <niek.janssen01@gmail.com>
2026-01-22 16:09:12 +0100

    Fix cmakelists

--- src/structures/src/String/String.h
+++ src/structures/src/String/String.h
@@ -2,15 +2,16 @@
 #define STDLIB__STR__H
 
 #include "sacinterface.h"
+#include <stdbool.h>
 
 typedef char* string;
 
-string SACtostring (string arr, sac_int length);
+string SACtostring (unsigned char* arr, sac_int length);
 string SACautotostring (SACarg *sarr);
-void SACstrmod (string str, sac_int pos, char c);
+void SACstrmod (string str, sac_int pos, unsigned char c);
 string SACstrins (string outer, sac_int pos, string inner);
 void SACstrovwt (string outer, sac_int pos, string inner);
-char SACstrsel (string str, sac_int pos);
+unsigned char SACstrsel (string str, sac_int pos);
 string SACstrcat (string fst, string snd);
 string SACstrncat (string fst, string snd, sac_int n);
 sac_int SACstrcmp (string fst, string snd);
@@ -18,18 +19,18 @@ sac_int SACstrncmp (string fst, string snd, sac_int n);
 sac_int SACstrcasecmp (string fst, string snd);
 sac_int SACstrncasecmp (string fst, string snd, sac_int n);
 sac_int SACstrlen (string fst);
-char SACstrtake(string str, sac_int pos);
+void SACstrtake(string str, sac_int pos);
 string SACstrdrop (string str, sac_int pos);
 string SACstrext (string str, sac_int pos, sac_int len);
 string SACsprintf (string format, ...);
 sac_int SACsscanf( string s, string format, ...);
 string SACsscanf_str (string s, string format);
-sac_int SACstrchr (string str, char c);
-sac_int SACstrrchr (string str, char c);
+sac_int SACstrchr (string str, unsigned char c);
+sac_int SACstrrchr (string str, unsigned char c);
 sac_int SACstrcspn(string str, string reject);
 sac_int SACstrspn(string str, string accept);
 sac_int SACstrstr (string haystack, string needle);
-bool chr_in_delims (char c, string delimiters);
+bool chr_in_delims (unsigned char c, string delimiters);
 string next_in_delims (string str, string delimiters, bool is_delimiter);
 void SACstrtok (string* out_token, string* out_rest, string str, string delimiters);
 void SACchomp (string str) ;
