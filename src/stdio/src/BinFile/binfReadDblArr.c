81b129ad51aba04e31246e5e50d08806d4c4480d
Thomas Koopman <thomas.koopman@ru.nl>
2026-01-30 14:43:02 +0100

    BinFile was not done. Breaks on Debian 13

--- src/stdio/src/BinFile/binfReadDblArr.c
+++ src/stdio/src/BinFile/binfReadDblArr.c
@@ -5,41 +5,18 @@
 
 #include "BinFile.h"
 
-#define array_nt (array, T_OLD((AUD, (NHD, (NUQ, )))))
-#define ret_nt   (ret,   T_OLD((AUD, (NHD, (NUQ, )))))
-
-void SACbinfReadDoubleArray( SAC_ND_PARAM_out( array_nt, double), int fd, int dim, int *shp)
+SACarg *SACbinfReadDoubleArray(sac_int fd, sac_int dim, sac_int *shp)
 {
-  SAC_ND_DECL__DATA( ret_nt, double, )
-  SAC_ND_DECL__DESC( ret_nt, )
-  int SAC_ND_A_MIRROR_DIM( ret_nt) = dim;
-  int i, size;
-  int bytesRead;
   double *doublearray;
-  int given_dim;
-  int *given_shp;
 
-  SAC_ND_ALLOC__DESC( ret_nt, dim)
-  SAC_ND_SET__RC( ret_nt, 1)
-
-    /*  start_token = PARSE_DOUBLE_ARRAY; */
-    /*  yyin = stream; */
-  given_dim = dim;
-  given_shp = shp;
-  for( i = 0, size = 1; i < dim; i++) {
+  sac_int size = 1;
+  for (sac_int i = 0; i < dim; i++) {
     size *= shp[i];
-    SAC_ND_A_DESC_SHAPE( ret_nt, i) = shp[i];
   }
-  SAC_ND_A_DESC_SIZE( ret_nt) = size;
-  doublearray = (double *) SAC_MALLOC( size * sizeof( double));
+  doublearray = (double *)malloc(size * sizeof(double));
 
   /* read double array from file fd */
-  bytesRead = read(fd,doublearray,size * sizeof(double));
+  ssize_t bytesRead = read((int)fd, doublearray, size * sizeof(double));
 
-  SAC_ND_A_FIELD( ret_nt) = doublearray;
-
-  SAC_ND_RET_out( array_nt, ret_nt)
+  return SACARGcreateFromPointer(SACTYPE__MAIN__double, doublearray, dim, shp);
 }
-
-#undef array_nt
-#undef ret_nt
--- src/stdio/src/BinFile/binfWriteDblArr.c
+++ src/stdio/src/BinFile/binfWriteDblArr.c
@@ -5,24 +5,18 @@
 
 #include "BinFile.h"
 
-#define array_nt (array, (AUD, (NHD, (NUQ, ))))
-#define ret_nt   (ret,   (AUD, (NHD, (NUQ, ))))
-
-void SACbinfWriteDoubleArray(int fd, int dim, int *shp, double* array)
+void SACbinfWriteDoubleArray(sac_int fd, sac_int dim, sac_int *shp, double *array)
 {
-  int i;
-  size_t size;
-  ssize_t res; // the return value of write can be negative!
-  for( i = 0, size = 1; i < dim; i++) {
+  sac_int size = 1;
+  for (sac_int i = 0; i < dim; i++) {
     size *= shp[i];
   }
 
-  res = write(fd,array,size*sizeof(double));
-  if( res != (ssize_t) (size*sizeof(double))) {
-    SAC_RuntimeWarning( "only managed to write %d bytes of a %d byte array of doubles",
-                         res, size*sizeof(double));
+  ssize_t res = write((int)fd, array, size*sizeof(double));
+
+  if (res != (ssize_t)(size * sizeof(double))) {
+    SAC_RuntimeWarning("only managed to write %zd bytes of a" 
+                       "%" PRIisac " byte array of doubles",
+                       res, size * sizeof(double));
   }
 }
-
-#undef array_nt
-#undef ret_nt
